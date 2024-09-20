609658978cf2a21c54c045ddc1d765a7198ecaae
Darshan11777 <dkanani405405@gmail.com>
2024-09-20 16:31:55 +0530

    trying to add admin panel scss with

--- web pro/src/App.jsx
+++ web pro/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/homePage/Home'
 import Navbar from './component/Navbar';
 import AboutUs from './pages/aboutUs/AboutUs';
+import AdminPanel from './pages/AdminPanel/src/App';
 
 export default function App() {
   return (
@@ -13,6 +14,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="about-us" element={<AboutUs />} />
+        {/* <Route path="admin" element={<AdminPanel/>} /> */}
 
         {/* Add more routes as needed */}
       </Routes>
